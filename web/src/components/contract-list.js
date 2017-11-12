23a0fab2fee1d73eef5efbb6ca5876597bacf064
Liana <lia.gukasyan@gmail.com>
2017-11-12 14:01:14 +0300

    Add new contract link

--- web/src/components/contract-list.js
+++ web/src/components/contract-list.js
@@ -5,18 +5,37 @@ import {Link} from 'react-router-dom'
 import connect from '~/utils/connect'
 import sel from '~/selectors'
 
+import plus_circle from '../../assets/plus_circle.svg'
+
 import ContractLayout from './freelancer-layout'
 
+const Header = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`
 const FormTitle = styled.h2`
   color: #242737;
   font-size: 36px;
+`
+const NewContract = styled.a`
+	text-decoration: none;
+	color: black;
+	&:before {
+		content: url(${plus_circle});
+    vertical-align: -25%;
+    padding-right: 5px;
+	}
 `
 
 export class ContractList extends React.Component {
 	render() {
 		return (
 			<ContractLayout>
-				<FormTitle>Contract List</FormTitle>
+				<Header>
+					<FormTitle>Contract List</FormTitle>
+					<NewContract href='/new'>New Contract</NewContract>
+				</Header>
 			</ContractLayout>	
 		)
 	}
